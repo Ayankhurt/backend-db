f8d597405dbad3b4d19b6d2af146c9ddd3adb68f
Ayan Khurt <ayankhurt@gmail.com>
2025-05-23 00:12:55 +0500

    new

---
+++ db.js
@@ -0,0 +1,10 @@
+import{ Pool } from "pg"
+import 'dotenv/config'
+
+export const db = new Pool({
+  user: process.env.DB_USERS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
