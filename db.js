d9621d13f54bd4240cd416c0803bd453119b2982
Ayan Khurt <ayankhurt@gmail.com>
2025-05-23 00:58:57 +0500

    new

--- db.js
+++ db.js
@@ -7,22 +7,5 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  connectionTimeoutMillis: 5000,
-  idleTimeoutMillis: 5000,
-  max: 20
 })
-
-// Test the database connection
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
-})
-
-pool.on('connect', () => {
-  console.log('Database connected successfully')
-})
-
 export const db = pool
